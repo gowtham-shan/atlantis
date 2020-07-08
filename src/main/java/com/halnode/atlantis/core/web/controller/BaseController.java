521af3e3f5672b1d697fef65e11deb0c17d13d11
Gowtham_Shan <gowthamshann@gmail.com>
2020-07-08 17:44:45 +0530

    Query changes for user retrival

--- src/main/java/com/halnode/atlantis/core/web/controller/BaseController.java
+++ src/main/java/com/halnode/atlantis/core/web/controller/BaseController.java
@@ -50,6 +50,5 @@ public class BaseController {
         } catch (Exception e) {
             return null;
         }
-
     }
 }
--- src/main/java/com/halnode/atlantis/spring/authentication/UserDetailsServiceImpl.java
+++ src/main/java/com/halnode/atlantis/spring/authentication/UserDetailsServiceImpl.java
@@ -16,7 +16,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Query;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Service
@@ -30,6 +29,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String userIdentifier) throws UsernameNotFoundException {
         User user = null;
+
+        //Separate user_name and org_name(i.e., SCHEMA NAME) from the input
         String currentUserName = userIdentifier.substring(0, userIdentifier.lastIndexOf("@"));
         String currentOrgName = userIdentifier.substring(userIdentifier.lastIndexOf("@") + 1, userIdentifier.length());
         EntityManager entityManager = entityManagerFactory.createEntityManager();
@@ -37,27 +38,23 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         entityManager.getTransaction().begin();
         query = entityManager.createNativeQuery(String.format("SET SCHEMA \'%s\';", currentOrgName));
         query.executeUpdate();
-        String sql = "SELECT u FROM User u";
+
+        //Get the user from the database for the given user_name
+        String sql = "SELECT u FROM User u WHERE u.userName=:userName";
         Query user_query = entityManager.createQuery(sql);
-        List<User> users = user_query.getResultList();
-        User user1 = null;
-        for (User u : users) {
-            if (u.getUserName().equals(currentUserName)) {
-                user1 = u;
-            }
-        }
+        user_query.setParameter("userName", currentUserName);
+        user = (User) user_query.getSingleResult();
         entityManager.getTransaction().commit();
         entityManager.close();
-        if (ObjectUtils.isEmpty(user1)) throw new UsernameNotFoundException(currentUserName);
+        if (ObjectUtils.isEmpty(user)) throw new UsernameNotFoundException(currentUserName);
 
+        //Set roles from the user objects
         Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
-        if (user1 != null) {
-            user1.getRoles().forEach(role -> {
-                System.out.println("Matched User Role : " + role.getName());
+        if (!ObjectUtils.isEmpty(user.getRoles())) {
+            user.getRoles().forEach(role -> {
                 grantedAuthorities.add(new SimpleGrantedAuthority(role.getName()));
             });
         }
-
-        return new org.springframework.security.core.userdetails.User(user1.getUserName(), user1.getPassword(), grantedAuthorities);
+        return new org.springframework.security.core.userdetails.User(user.getUserName(), user.getPassword(), grantedAuthorities);
     }
 }
